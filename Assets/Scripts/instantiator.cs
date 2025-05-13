6e4d2fafe770fa7edbe40f80faa54321787d3c21
LucasPar08 <lucaspark2008@gmail.com>
2025-05-13 11:59:50 -0300

    Terminado

---
+++ Assets/Scripts/instantiator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instantiator : MonoBehaviour
+
+{
+
+    public GameObject cuboPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(cuboPrefab);
+    }
+
+    public void InstanciarCubo()
+    {
+        Instantiate(cuboPrefab);
+    }
+}
